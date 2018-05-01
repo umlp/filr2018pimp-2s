736517addb2a7d5f2857cdafcf273f8b26aa8909
QuentinLautridou <36047586+QuentinLautridou@users.noreply.github.com>
2018-05-01 18:59:10 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-offset-3 col-sm-7">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     New Task
