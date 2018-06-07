b5751a2459770aa9ec3aefd6999d77f2ab461d87
QuentinLautridou <36047586+QuentinLautridou@users.noreply.github.com>
2018-06-07 17:51:44 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-offset-2 col-sm-8 formulaire">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     Nouvelle soirée
